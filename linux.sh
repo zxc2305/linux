0197611918efdd0e50120df6d5e6b2233fe88876
java-zlp <java-zlp@example.com>
2019-07-17 01:29:43 +0800

    linux.sh

---
+++ linux.sh
@@ -0,0 +1,12 @@
+yum  install  -y  java-1.8.0-openjdk   java-1.8.0-openjdk-devel
+ echo "#set java environment
+JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64
+PATH=$PATH:$JAVA_HOME/bin
+CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export JAVA_HOME CLASSPATH PATH
+" >>/etc/profile
+source /etc/profile
+cd /root
+git clone git@github.com:zxc2305/tomcat.git
+cd /root/tomcat/apache-tomcat-9.0.21/bin
+nohup ./catalina.sh run &
